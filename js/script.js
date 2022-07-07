119dc5d497ad0995e6c4a42a965bd1598eee844b
Bruno C. Gomes Da Silva <73727546+BruiinSilva@users.noreply.github.com>
2022-07-06 21:34:59 -0300

    combustivel app

--- js/script.js
+++ js/script.js
@@ -1,38 +1,40 @@
-
 function combustivel() {
-    // declaração de variáveis
+  // declaração de variáveis
+  var gasolina    = document.getElementById('gasolina').value;
+  var etanol      = document.getElementById('etanol').value;
+  var result      = document.getElementById('result');
+
+  // substituir virgula por ponto
+  gasolina        = gasolina.replace(',', '.');
+  etanol          = etanol.replace(',', '.');
+
+  if(gasolina != '' && etanol != '') {
+      // calcular o melhor combustível
+      var maisBarato  = etanol/gasolina;
+
+      //algoritmo
+    if (maisBarato >= 0.7) {
+      result.innerHTML = 'Melhor abastecer com Gasolina.';
+      result.style = 'color: #000; text-shadow: 2px 2px 2px #ff0';
+    } 
     
-    var gasolina    = document.getElementById("gasolina").value;
-    var etanol      = document.getElementById("etanol").value;
-    var result      = document.getElementById("result");
-  
-    gasolina        = gasolina.replace(",", ".");
-    etanol          = etanol.replace(",", ".");
-  
-    var maisBarato  = etanol/gasolina;
-  
-    // algoritmo
-    if(gasolina != "" && etanol != "") {
-      if (maisBarato >= 0.7) {
-        result.innerHTML = "Melhor abastecer com Gasolina.";
-        result.style = 'color: #000; text-shadow: 2px 2px 2px #ff0';
-      } else {
-        result.innerHTML = "Melhor abastecer com Etanol.";
-        result.style = 'color: #000; text-shadow: 2px 2px 2px rgb(221, 25, 25)';
-      }
-    } else {
-      alert("Preencha todos os campos!")
+    else {
+      result.innerHTML = 'Melhor abastecer com Etanol.';
+      result.style = 'color: rgb(221, 25, 25); text-shadow: 2px 2px 2px rgb(221, 25, 25)';
     }
+  } 
+  else {
+    alert('Preencha todos os campos!');
   }
-  
-  function verificarInput(event) {
-    var num = event.key
-  
-    if (num != 0 && num != 1 && num != 2 && num != 3 && num != 4 && num != 5 && num != 6 && num != 7 && num != 8 && num != 9 && letra != ',' && letra != '.' && letra != 'Tab' && letra != 'Enter' && letra != 'Backspace' && letra != 'Delete' && letra != 'ArrowLeft' && letra != 'ArrowRight') {
-        alert("Caractere inválido!")
-        
-      document.getElementById("gasolina").value = "";
-      document.getElementById("etanol").value = "";
-    }
+}
+
+function verificarInput(event) {
+  var letra = event.key;
+
+  if (letra != 0 && letra != 1 && letra != 2 && letra != 3 && letra != 4 && letra != 5 && letra != 6 && letra != 7 && letra != 8 && letra != 9 && letra != ',' && letra != '.' && letra != 'Tab' && letra != 'Enter' && letra != 'Backspace' && letra != 'Delete' && letra != 'ArrowLeft' && letra != 'ArrowRight') {
+    alert('Caractere inválido!');
+    document.getElementById('gasolina').value = '';
+    document.getElementById('etanol').value = '';
   }
+}
 
